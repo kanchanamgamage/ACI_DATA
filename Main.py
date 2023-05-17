6a5255598e055d15c79260ffb802327ddacd8d8e
kanchanamgamage <45864789+kanchanamgamage@users.noreply.github.com>
2023-05-17 13:14:47 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,52 +1,43 @@
-import openpyxl
-
+import csv
 
 with open('example.txt', 'r') as file:
-    content = file.readlines()
+    content = file.read()
 
-workbook = openpyxl.Workbook()
+# Split the content into individual fault.Inst sections
+fault_insts = content.split('# fault.Inst')
 
-worksheet = workbook.active
+# Remove empty fault.Inst sections
+fault_insts = [fault_inst for fault_inst in fault_insts if fault_inst.strip()]
 
-worksheet.append(['Code', 'Ack', 'Cause', 'Description', 'Domain', 'Severity', 'Last Transition'])
+# Get all unique field names
+field_names = set()
+for fault_inst in fault_insts:
+    lines = fault_inst.strip().split('\n')
+    for line in lines:
+        if ':' in line:
+            field = line.split(':')[0].strip()
+            field_names.add(field)
 
-code = ''
-ack = ''
-cause = ''
-description = ''
-domain = ''
-severity = ''
-last_transition = ''
+# Create a CSV file for the output
+with open('output.csv', 'w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=field_names)
+    writer.writeheader()
 
-for line in content:
+    # Process each fault.Inst section
+    for fault_inst in fault_insts:
+        # Create a dictionary to store the field values
+        field_data = {}
 
-    if line.startswith('code'):
-        code = line.split(': ')[1].strip()
+        # Split the fault.Inst section into lines
+        lines = fault_inst.strip().split('\n')
 
-    elif line.startswith('ack'):
-        ack = line.split(': ')[1].strip()
+        # Extract the field name-value pairs
+        for line in lines:
+            if ':' in line:
+                field, value = line.split(':', 1)
+                field = field.strip()
+                value = value.strip()
+                field_data[field] = value
 
-    elif line.startswith('cause'):
-        cause = line.split(': ')[1].strip()
-
-    elif line.startswith('descr'):
-        description = line.split(': ')[1].strip()
-
-    elif line.startswith('domain'):
-        domain = line.split(': ')[1].strip()
-
-    elif line.startswith('severity'):
-        severity = line.split(': ')[1].strip()
-
-    elif line.startswith('lastTransition'):
-        last_transition = line.split(': ')[1].strip()
-        worksheet.append([code, ack, cause, description, domain, severity, last_transition])
-        code = ''
-        ack = ''
-        cause = ''
-        description = ''
-        domain = ''
-        severity = ''
-        last_transition = ''
-
-workbook.save('example.xlsx')
+        # Write the field values to the CSV file
+        writer.writerow(field_data)
